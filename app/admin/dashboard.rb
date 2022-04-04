b8cab3fb4b9e9bb506f198ef45e8dfb23a1bb283
chaky <chaky22222222@gmail.com>
2022-04-04 21:48:51 +0300

    fixed admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,77 +4,7 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     columns do
       column do
-        extend ActiveAdmin::EventHelper
 
-        if Event.published.exists?
-          panel "Events stats" do
-
-            table do
-              thead do
-                tr do
-                  %w[Minimun_Price Maximum_Price Average_Price].each &method(:th)
-                end
-              end
-              tbody do
-                tr do
-                  td do
-                    minimum_event_price
-                  end
-                  td do
-                    maximum_event_price
-                  end
-                  td do
-                    average_event_price
-                  end
-                end
-              end
-            end
-
-            table do
-              thead do
-                tr do
-                  %w[not_published_events published_events canceled_events finished_events].each &method(:th)
-                end
-              end
-              tbody do
-                tr do
-                  td do
-                    not_published_events
-                  end
-                  td do
-                    published_events
-                  end
-                  td do
-                    canceled_events
-                  end
-                  td do
-                    finished_events
-                  end
-                end
-              end
-            end
-
-            table do
-              thead do
-                tr do
-                  %w[location quantity].each &method(:th)
-                end
-              end
-              tbody do
-                groupped_events_by_geography.each do |g_str, g_hash|
-                  tr do
-                    td do
-                      g_str
-                    end
-                    td do
-                      g_hash.size
-                    end
-                  end
-                end
-              end
-            end
-          end
-        end
       end
 
       column do
@@ -86,7 +16,9 @@ ActiveAdmin.register_page "Dashboard" do
             table do
               thead do
                 tr do
-                  %w[Minimun_Age Maximum_Age Average_Age].each &method(:th)
+                  th do
+                    No data
+                  end
                 end
               end
               tbody do
